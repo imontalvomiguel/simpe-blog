969e82358fbf1281e7a247e2991cbb5bbd74a6e8
imontalvomiguel <ubaldo.im@gmail.com>
2015-07-31 00:11:13 -0500

    unused dependencies

--- index.php
+++ index.php
@@ -8,9 +8,6 @@ require_once 'library/Response.php';
 require_once 'library/View.php';
 require_once 'library/Database.php';
 
-use Blog\DB as Database;
-use Blog\Helpers as Helpers;
-
 $requestUrl = new RequestUrl($_GET['url']);
 $request = new Request($requestUrl);
 $request->execute();
