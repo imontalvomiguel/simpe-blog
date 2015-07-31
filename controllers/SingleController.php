a3297dc8f46f5fdf85398c51eafcf0bfc5bc0d03
imontalvomiguel <ubaldo.im@gmail.com>
2015-07-31 02:03:37 -0500

    Update registers

--- controllers/SingleController.php
+++ controllers/SingleController.php
@@ -20,4 +20,37 @@ class SingleController {
     }
   }
 
+  public function editAction($id)
+  {
+    $db = new Blog\DB\Database();
+    $post = $db->findId( intval($id), 'posts');
+    if ($post)
+    {
+      return $response = new View( 'single-edit', $post);
+    } else {
+      return false;
+    }
+  }
+
+  public function updateAction($id)
+  {
+    extract( $_POST );
+
+    if ( empty($title) || empty($body) ) {
+      $status = 'Title & Body are required.';
+    } else {
+      $db = new Blog\DB\Database();
+      $db->query('UPDATE posts SET title = :title, body = :body WHERE id = :id', [':title' => $title, ':body' => $body, ':id' => $id]);
+      $status = 'Updated';
+    }
+
+    return $response = new View('single-edit', array(
+      'status' => $status,
+      'title' => $title,
+      'body' => $body,
+      'id' => $id
+    ));
+
+  }
+
 }
